00485a9882a04b31a7a7dc027191f08b86ebb650
Jesse Thach <jessethach@gmail.com>
2021-07-14 10:14:39 -0700

    Update packages and gatsby core.

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,6 +63,6 @@ module.exports = {
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-netlify-cms`,
     `gatsby-plugin-styled-components`,
-    'gatsby-plugin-typescript'
+    'gatsby-plugin-typescript',
   ],
 };
