de35ee2192e8e56e30715466ca93a1551f7def49
Jesse Thach <jessethach@gmail.com>
2021-08-09 13:46:14 -0700

    Remove package. Add loading string to component.

--- src/pages/state/[state].tsx
+++ src/pages/state/[state].tsx
@@ -5,14 +5,19 @@ import { findGeoState } from '../../utils/util';
 
 import Layout from '../../components/layout/Layout';
 
-export default function State({ params: { state = '' } }: PageProps) {
+export default function State({ params: { state = 'loading' } }: PageProps) {
   const { statesData } = useDataStates();
+  console.log(state);
 
   return (
     <Layout>
-      <p data-cy="state-text">
-        {findGeoState(statesData, state.toUpperCase())?.name}
-      </p>
+      {state == 'loading' ? (
+        <h2>Loading...</h2>
+      ) : (
+        <p data-cy="state-text">
+          {findGeoState(statesData, state.toUpperCase())?.name}
+        </p>
+      )}
     </Layout>
   );
 }
