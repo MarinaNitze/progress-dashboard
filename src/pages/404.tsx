d5b80269e8aa3eb0c0427e33e8b640316653b0d3
Jesse Thach <jessethach@gmail.com>
2021-08-09 14:06:56 -0700

    Remove loading logic in component. Add logic to 404 component.

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
 import Layout from '../components/layout/Layout';
 import Seo from '../components/seo/Seo';
 
 export default function NotFoundPage() {
+  const [isMount, setMount] = useState(false);
+
+  useEffect(() => {
+    setMount(true);
+  }, []);
+
+  if (!isMount) {
+    return <h2>Loading...</h2>;
+  }
+
   return (
     <Layout>
       <Seo title="404: Not found" />
--- src/pages/state/[state].tsx
+++ src/pages/state/[state].tsx
@@ -5,19 +5,14 @@ import { findGeoState } from '../../utils/util';
 
 import Layout from '../../components/layout/Layout';
 
-export default function State({ params: { state = 'loading' } }: PageProps) {
+export default function State({ params: { state = '' } }: PageProps) {
   const { statesData } = useDataStates();
-  console.log(state);
 
   return (
     <Layout>
-      {state == 'loading' ? (
-        <h2>Loading...</h2>
-      ) : (
-        <p data-cy="state-text">
-          {findGeoState(statesData, state.toUpperCase())?.name}
-        </p>
-      )}
+      <p data-cy="state-text">
+        {findGeoState(statesData, state.toUpperCase())?.name}
+      </p>
     </Layout>
   );
 }
