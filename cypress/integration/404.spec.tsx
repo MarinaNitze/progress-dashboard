5fb4d257924b4744f7688c537f6a1ffead575043
Jesse Thach <jessethach@gmail.com>
2021-08-17 12:46:19 -0700

    Add pre push.

--- cypress/integration/404.spec.tsx
+++ cypress/integration/404.spec.tsx
@@ -4,7 +4,6 @@ describe('State page', () => {
   it('should successfully navigate to Washington state page', () => {
     cy.visit('/state/not-a-real-param');
 
-    cy.get('header').should('have.text', 'Progress Dashboard');
     cy.get('[data-cy=404-heading]').should('have.text', '404');
     cy.get('[data-cy=404-text]').should('have.text', 'Not found');
   });
