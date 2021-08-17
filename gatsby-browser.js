58eb5a0d604cfb55f7f706d11d7bd7a0d0729cad
Jesse Thach <jessethach@gmail.com>
2021-08-17 10:36:17 -0700

    Fix mobile nav bug.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,2 @@
 import '@trussworks/react-uswds/lib/index.css';
 import './src/styles/index.scss';
-import 'uswds';
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,46 +1,49 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import {
   Header as HeaderCmp,
   Title,
   NavMenuButton,
   PrimaryNav,
 } from '@trussworks/react-uswds';
-import { useState } from 'react';
+
+const itemsMenu = [
+  <a href="#one" key="one" className="usa-nav__link">
+    <span>Topics</span>
+  </a>,
+  <a href="#two" key="two" className="usa-nav__link">
+    <span>Recommendations</span>
+  </a>,
+  <a href="#three" key="three" className="usa-nav__link">
+    <span>Compare</span>
+  </a>,
+  <a href="#four" key="four" className="usa-nav__link">
+    <span>Stories</span>
+  </a>,
+  <a href="#five" key="five" className="usa-nav__link">
+    <span>Search</span>
+  </a>,
+];
 
 export default function Header() {
   const [expanded, setExpanded] = useState(false);
-  const onClick = (): void => setExpanded(prvExpanded => !prvExpanded);
 
-  const itemsMenu = [
-    <a href="#one" key="one" className="usa-nav__link">
-      <span>Topics</span>
-    </a>,
-    <a href="#two" key="two" className="usa-nav__link">
-      <span>Recommendations</span>
-    </a>,
-    <a href="#three" key="three" className="usa-nav__link">
-      <span>Compare</span>
-    </a>,
-    <a href="#four" key="four" className="usa-nav__link">
-      <span>Stories</span>
-    </a>,
-    <a href="#five" key="five" className="usa-nav__link">
-      <span>Search</span>
-    </a>,
-  ];
+  const onClickExpand = useCallback(() => {
+    setExpanded(prvExpanded => !prvExpanded);
+  }, [setExpanded]);
+
   return (
     <>
       <div className={`usa-overlay ${expanded ? 'is-visible' : ''}`}></div>
-      <HeaderCmp basic={true}>
+      <HeaderCmp basic>
         <div className="usa-nav-container">
           <div className="usa-navbar">
             <Title>Logo</Title>
-            <NavMenuButton onClick={onClick} label="Menu" />
+            <NavMenuButton onClick={onClickExpand} label="Menu" />
           </div>
           <PrimaryNav
             items={itemsMenu}
             mobileExpanded={expanded}
-            onToggleMobileNav={onClick}
+            onToggleMobileNav={onClickExpand}
           ></PrimaryNav>
         </div>
       </HeaderCmp>
