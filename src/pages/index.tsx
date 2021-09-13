07f3d845a3138fcc39cf732860fd3dbfb222c10b
SammySteiner <sammysteiner@gmail.com>
2021-09-13 15:01:09 -0400

    tried to add theme colors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,7 +23,7 @@ const IndexPage: React.FC<{ data: ImagesQuery }> = ({ data }) => {
           <GridContainer>
             <Hero
               backgroundImg={heroImage}
-              backgroundColor="primary"
+              backgroundColor="bg-primary"
               title={'[Playbook Tagline]'}
               description={
                 'Quick intro that sets the stage for the site, what people can do, and how to do it.'
