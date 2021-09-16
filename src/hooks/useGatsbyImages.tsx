2307774d71f962182efa193641431f6ddcb95487
Jesse Thach <jessethach@gmail.com>
2021-09-15 17:31:54 -0700

    Add hook changes.

--- src/hooks/useGatsbyImages.tsx
+++ src/hooks/useGatsbyImages.tsx
@@ -6,63 +6,65 @@ type useGatsbyImageResponse = {
 };
 
 export default function useGatsbyImages(): useGatsbyImageResponse {
-  const {
-    heroImages,
-    topicImages,
-    featureImages,
-    headerImages,
-  } = useStaticQuery<HeroAndCardImagesQuery>(graphql`
-    query HeroAndCardImages {
-      heroImages: allFile(
-        filter: { relativeDirectory: { eq: "images/heros" } }
-      ) {
-        edges {
-          node {
-            relativePath
-            childImageSharp {
-              gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+  const { heroImages, topicImages, featureImages, headerImages } =
+    useStaticQuery<HeroAndCardImagesQuery>(graphql`
+      query HeroAndCardImages {
+        heroImages: allFile(
+          filter: { relativeDirectory: { eq: "images/heros" } }
+        ) {
+          edges {
+            node {
+              relativePath
+              childImageSharp {
+                gatsbyImageData(
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP, AVIF]
+                )
+              }
+              extension
             }
-            extension
           }
         }
-      }
-      featureImages: allFile(
-        filter: { relativeDirectory: { eq: "images/features" } }
-      ) {
-        edges {
-          node {
-            relativePath
-            childImageSharp {
-              gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+        featureImages: allFile(
+          filter: { relativeDirectory: { eq: "images/features" } }
+        ) {
+          edges {
+            node {
+              relativePath
+              childImageSharp {
+                gatsbyImageData(
+                  placeholder: BLURRED
+                  formats: [AUTO, WEBP, AVIF]
+                )
+              }
+              extension
             }
-            extension
+          }
+        }
+        topicImages: allFile(
+          filter: { relativeDirectory: { eq: "images/topics" } }
+        ) {
+          edges {
+            node {
+              relativePath
+              extension
+              publicURL
+            }
+          }
+        }
+        headerImages: allFile(
+          filter: { relativeDirectory: { eq: "images/header" } }
+        ) {
+          edges {
+            node {
+              relativePath
+              extension
+              publicURL
+            }
           }
         }
       }
-      topicImages: allFile(
-        filter: { relativeDirectory: { eq: "images/topics" } }
-      ) {
-        edges {
-          node {
-            relativePath
-            extension
-            publicURL
-          }
-        }
-      }
-      headerImages: allFile(
-        filter: { relativeDirectory: { eq: "images/header" } }
-      ) {
-        edges {
-          node {
-            relativePath
-            extension
-            publicURL
-          }
-        }
-      }
-    }
-  `);
+    `);
 
   const images = [heroImages, topicImages, featureImages, headerImages].reduce(
     (acc, images) => {
