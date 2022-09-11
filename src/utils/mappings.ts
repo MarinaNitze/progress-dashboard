e1db1557db3ad8669a4cef35e4587795d7d04108
julia <hogan.julia@gmail.com>
2022-09-11 19:21:16 -0400

    Use full titles in compare index page

---
+++ src/utils/mappings.ts
@@ -0,0 +1,7 @@
+import { Topic } from '../types/compare';
+
+export const COMPARE_TOPIC_FULL_TITLE: Record<Topic, string> = {
+  'Background Checks':
+    'Out of State Child Abuse and Neglect Checks (Adam Walsh Checks)',
+  'Family Finding': 'Kin Finding',
+};
