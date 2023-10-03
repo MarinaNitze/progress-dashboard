7aa65c6ae4ab5a541a18c1a69d66f3184efd6604
Marina Nitze <marinanitze@marinas.macbook.air.lan>
2023-10-03 08:05:49 -0700

    Tweaking rule page

--- src/pages/kinship-caregivers.tsx
+++ src/pages/kinship-caregivers.tsx
@@ -12,7 +12,7 @@ A [new rule](https://www.federalregister.gov/documents/2023/09/28/2023-21081/sep
 
 A title IV-E agency’s kin-specific licensing or approval standards must be “reasonably in accord with recommended standards of national organizations.”
 
-The new [Kin-Specific Foster Home Approval Standards](https://docs.google.com/document/d/1V30W6Ft_uEUpVIQ062wPh12HrYHNod0b43T25wrp190/edit) will serve as these “recommended standards of national organizations,” similar to how the [Model Foster Home Licensing Standards](https://nara.memberclicks.net/nara-model-foster-care-standards) served as the recommended standards for the previous combined (kin and non-kin) process. 
+**The new [Kin-Specific Foster Home Approval Standards](https://docs.google.com/document/d/1V30W6Ft_uEUpVIQ062wPh12HrYHNod0b43T25wrp190/edit)** will serve as these “recommended standards of national organizations,” similar to how the [Model Foster Home Licensing Standards](https://nara.memberclicks.net/nara-model-foster-care-standards) served as the recommended standards for the previous combined (kin and non-kin) process. 
 
 The Kin-Specific Foster Home Approval Standards respect the unique circumstances of kin caregivers, removing many historic barriers and striving to make the approval process warmer and more flexible for kin caregivers. We worked closely with kin caregivers, subject matter experts, and at least 45 title IV-E agencies to develop these model standards and implementation guidance.
 
@@ -30,9 +30,6 @@ const GET_INVOLVED = `
 - [Sign up for email updates](https://mailchi.mp/childwelfareplaybook/standards-for-kinship-caregivers)
 - [Sign up for the launch webinar](https://www.gksnetwork.org/events/kin-specific-licensure-overview-of-new-federal-rule-release-of-recommended-standards/) on Wednesday, October 11 from 1-2p PT / 4-5p ET. The event will be recorded if you cannot attend live. 
 - Share your feedback and questions on the model standards
-`;
-
-const MORE_WAYS = `
 `;
 
 const FAQ = `
@@ -89,10 +86,6 @@ export default function KinshipCaregivers() {
                 title: 'Get involved',
               },
               {
-                to: '#more-ways',
-                title: 'More ways to get involved',
-              },
-              {
                 to: '#faq',
                 title: 'FAQ',
               },
@@ -117,12 +110,6 @@ export default function KinshipCaregivers() {
               <section>
                 <h2 className="section-title">Get involved!</h2>
                 <ReactMarkdown>{GET_INVOLVED}</ReactMarkdown>
-              </section>
-            </Grid>
-            <Grid id="more-ways">
-              <section>
-                <h2 className="section-title">More ways to get involved</h2>
-                <ReactMarkdown>{MORE_WAYS}</ReactMarkdown>
               </section>
             </Grid>
             <Grid id="faq">
